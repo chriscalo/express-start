712203959fd28bd922f36c19a73b19ed960527fd
Chris Calo <chris.calo@gmail.com>
2019-09-10 22:52:43 -0400

    test: remove chai

--- index.test.js
+++ index.test.js
@@ -1,27 +1,25 @@
 import test from "ava";
-import { expect } from "chai";
 import express from "express";
-import portfinder from "portfinder";
 
 import { start } from "./index";
 import { findPort } from "./find-port";
 
-test("starts server", async t => {
+test("starts server", async ({ is, truthy }) => {
   const server = express();
   
   const listener = await start(server);
   const { port } = listener.address();
   
-  expect(port).to.be.a("number");
+  is(typeof port, "number");
 });
 
-test("starts server with specified port", async t => {
+test("starts server with specified port", async ({ is, truthy }) => {
   const server = express();
   const startPort = 3000;
   
   const listener = await start(server, startPort);
   const { port } = listener.address();
   
-  expect(port).to.be.a("number");
-  expect(port >= startPort).to.be.true;
+  is(typeof port, "number");
+  truthy(port >= startPort);
 });
