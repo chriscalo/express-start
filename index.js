7a2c33ac297a392ec443220127f1eaf2adb93458
Chris Calo <ccalo@google.com>
2019-12-16 13:53:03 -0500

    feature(start): copy URL to clipboard in dev mode

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import { findPort } from "./find-port";
 
 export async function start(server, startPort = 8000) {
+  const isDev = process.env.NODE_ENV !== "production";
   const PORT = process.env.PORT || await findPort(startPort);
   
-  const listener = await server.listen(PORT, () => {
+  const listener = await server.listen(PORT, async () => {
     console.log("");
-    console.log(`App listening on port ${PORT}`);
+    
+    if (isDev) {
+      const clipboardy = await import("clipboardy");
+      const url = `http://localhost:${PORT}/`;
+      clipboardy.writeSync(url);
+      console.log(`App listening at ${url}`);
+      console.log(`URL copied to clipboard.`);
+    } else {
+      console.log(`App listening on port ${PORT}`);
+    }
     console.log("Press Ctrl+C to quit.");
     console.log("");
   });
+  
   return listener;
 };
 
